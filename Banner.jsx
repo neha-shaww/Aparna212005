a5b062dd16febdef25f4b1fb20d9bd9cd5f6ce69
Aparna212005 <aparnabhakatt@gmail.com>
2024-10-19 21:22:29 +0530

    Add files via upload

---
+++ Banner.jsx
@@ -0,0 +1,9 @@
+import "./banner.css"
+const Banner=()=>{
+    return(
+        <div className="banner">
+            <div className="headline"></div>
+        </div>
+    )
+}
+export default Banner
